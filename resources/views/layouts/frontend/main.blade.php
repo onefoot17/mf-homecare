b964d480fd9337530f697fb2ecb9ae3ecd788995
Earl Misquitta <em@Earls-MacBook-Pro.local>
2020-11-30 19:07:13 -0500

    Adding DS_Store to gitignore and mf__ class to body tag

--- resources/views/layouts/frontend/main.blade.php
+++ resources/views/layouts/frontend/main.blade.php
@@ -37,7 +37,6 @@
 		<link rel="stylesheet" href="{{asset('porto/templates/css/theme-blog.css')}}">
 		<link rel="stylesheet" href="{{asset('porto/templates/css/theme-shop.css')}}">
 
-
 		<!-- Demo CSS -->
 		<link rel="stylesheet" href="{{asset('porto/templates/css/demos/demo-coffee.css')}}">
 		<!-- Skin CSS -->
@@ -51,120 +50,119 @@
 
 		<!-- Head Libs -->
 		<script src="{{asset('porto/templates/vendor/modernizr/modernizr.min.js')}}"></script>
-
 	</head>
-	<body data-target="#header" data-spy="scroll" data-offset="100">
 
-		<div class="body mf__{{ Request::segment( 2 ) }} mf__{{ Request::segment( 3 ) }}">
-			
+	<body class="@if ( !empty( Request::segment( 2 ) ) ) mf__{{ Request::segment( 2 ) }}@endif @if ( !empty( Request::segment( 3 ) ) ) mf__{{ Request::segment( 3 ) }}@endif" data-target="#header" data-spy="scroll" data-offset="100">
 
-<div class="owl-carousel-wrapper" style="height: 100vh;">
-	<div class="owl-carousel dots-inside dots-horizontal-center show-dots-hover show-dots-xs dots-light nav-style-1 nav-inside nav-inside-plus nav-light nav-md nav-font-size-md show-nav-hover mb-0" data-plugin-options="{'responsive': {'0': {'items': 1, 'dots': true, 'nav': false}, '479': {'items': 1}, '768': {'items': 1}, '979': {'items': 1}, '1199': {'items': 1}}, 'loop': false, 'autoHeight': false, 'margin': 0, 'dots': false, 'dotsVerticalOffset': '-125px', 'nav': true, 'animateIn': 'fadeIn', 'animateOut': 'fadeOut', 'mouseDrag': false, 'touchDrag': false, 'pullDrag': false, 'autoplay': false, 'autoplayTimeout': 7000, 'autoplayHoverPause': true, 'rewind': true}">
-						
-		<!-- Carousel Slide 1 -->
-		<div class="position-relative overlay overlay-show overlay-op-8" style="background-image: url({{asset('porto/templates/img/demos/coffee/slides/slide-1.jpg')}}); background-size: cover; background-position: center; height: 100vh;">
-			<div class="container position-relative z-index-3 h-100">
-				<div class="row justify-content-center align-items-center h-100">
-					<div class="col-lg-7 text-center">
-						<div class="d-flex flex-column align-items-center justify-content-center h-100">
-							<div class="overflow-hidden mb-2">
-								<h2 class="position-relative text-color-primary text-5 line-height-5 font-weight-normal ls-0 px-4 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">
-									WELCOME, ENJOY THE ORIGINAL
-								</h2>
+		<div class="body">
+			<div class="owl-carousel-wrapper" style="height: 100vh;">
+				<div class="owl-carousel dots-inside dots-horizontal-center show-dots-hover show-dots-xs dots-light nav-style-1 nav-inside nav-inside-plus nav-light nav-md nav-font-size-md show-nav-hover mb-0" data-plugin-options="{'responsive': {'0': {'items': 1, 'dots': true, 'nav': false}, '479': {'items': 1}, '768': {'items': 1}, '979': {'items': 1}, '1199': {'items': 1}}, 'loop': false, 'autoHeight': false, 'margin': 0, 'dots': false, 'dotsVerticalOffset': '-125px', 'nav': true, 'animateIn': 'fadeIn', 'animateOut': 'fadeOut', 'mouseDrag': false, 'touchDrag': false, 'pullDrag': false, 'autoplay': false, 'autoplayTimeout': 7000, 'autoplayHoverPause': true, 'rewind': true}">
+									
+					<!-- Carousel Slide 1 -->
+					<div class="position-relative overlay overlay-show overlay-op-8" style="background-image: url({{asset('porto/templates/img/demos/coffee/slides/slide-1.jpg')}}); background-size: cover; background-position: center; height: 100vh;">
+						<div class="container position-relative z-index-3 h-100">
+							<div class="row justify-content-center align-items-center h-100">
+								<div class="col-lg-7 text-center">
+									<div class="d-flex flex-column align-items-center justify-content-center h-100">
+										<div class="overflow-hidden mb-2">
+											<h2 class="position-relative text-color-primary text-5 line-height-5 font-weight-normal ls-0 px-4 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">
+												WELCOME, ENJOY THE ORIGINAL
+											</h2>
+										</div>
+										<div class="overflow-hidden mb-2">
+											<h1 class="text-color-light font-weight-extra-bold text-13 line-height-1 line-height-sm-3 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">FRESH PORTO COFFEE</h1>
+										</div>
+										<p class="text-4-5 text-color-light font-weight-light text-center px-lg-5 mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1800, 'minWindowWidth': 0, 'animationSpeed': 30}">The amazing flavors will blow your mind.</p>
+										<a href="#about" data-hash data-hash-offset="92" class="btn btn-primary btn-outline font-weight-bold text-color-light text-2 py-3 btn-px-4 mt-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="2700" data-plugin-options="{'minWindowWidth': 0}">LEARN ABOUT US</a>
+									</div>
+								</div>
 							</div>
-							<div class="overflow-hidden mb-2">
-								<h1 class="text-color-light font-weight-extra-bold text-13 line-height-1 line-height-sm-3 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">FRESH PORTO COFFEE</h1>
-							</div>
-							<p class="text-4-5 text-color-light font-weight-light text-center px-lg-5 mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1800, 'minWindowWidth': 0, 'animationSpeed': 30}">The amazing flavors will blow your mind.</p>
-							<a href="#about" data-hash data-hash-offset="92" class="btn btn-primary btn-outline font-weight-bold text-color-light text-2 py-3 btn-px-4 mt-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="2700" data-plugin-options="{'minWindowWidth': 0}">LEARN ABOUT US</a>
 						</div>
 					</div>
+
+					<!-- Carousel Slide 2 -->
+					<div class="position-relative overlay overlay-show overlay-op-8" style="background-image: url({{asset('porto/templates/img/demos/coffee/slides/slide-2.jpg')}}); background-size: cover; background-position: center; height: 100vh;">
+						<div class="container position-relative z-index-3 h-100">
+							<div class="row justify-content-center align-items-center h-100">
+								<div class="col-lg-7 text-center">
+									<div class="d-flex flex-column align-items-center justify-content-center h-100">
+										<div class="overflow-hidden mb-2">
+											<h3 class="position-relative text-color-primary text-5 line-height-5 font-weight-normal ls-0 px-4 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">
+												OUTSTANDING COFFEE
+											</h3>
+										</div>
+										<div class="overflow-hidden mb-2">
+											<h2 class="text-color-light font-weight-extra-bold text-13 line-height-1 line-height-sm-3 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">ESPRESSO BAR</h2>
+										</div>
+										<p class="text-4-5 text-color-light font-weight-light text-center px-lg-5 mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1800, 'minWindowWidth': 0, 'animationSpeed': 30}">Since 2001 with you.</p>
+										<a href="#about" data-hash data-hash-offset="92" class="btn btn-primary btn-outline font-weight-bold text-color-light text-2 py-3 btn-px-4 mt-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="2700" data-plugin-options="{'minWindowWidth': 0}">LEARN ABOUT US</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					
 				</div>
 			</div>
-		</div>
 
-		<!-- Carousel Slide 2 -->
-		<div class="position-relative overlay overlay-show overlay-op-8" style="background-image: url({{asset('porto/templates/img/demos/coffee/slides/slide-2.jpg')}}); background-size: cover; background-position: center; height: 100vh;">
-			<div class="container position-relative z-index-3 h-100">
-				<div class="row justify-content-center align-items-center h-100">
-					<div class="col-lg-7 text-center">
-						<div class="d-flex flex-column align-items-center justify-content-center h-100">
-							<div class="overflow-hidden mb-2">
-								<h3 class="position-relative text-color-primary text-5 line-height-5 font-weight-normal ls-0 px-4 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="500" data-plugin-options="{'minWindowWidth': 0}">
-									OUTSTANDING COFFEE
-								</h3>
+			<header id="header" class="header-narrow header-below-slider" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyStartAtElement': '#header', 'stickySetTop': '0', 'stickyChangeLogo': false}">
+				<div class="header-body border-0 box-shadow-none bg-dark appear-animation" data-appear-animation="customHeaderAnimOne">
+					<div class="header-container container">
+						<div class="header-row">
+							<div class="header-column justify-content-start d-lg-none">
+								<div class="header-row mt-2">
+									<div class="header-logo">
+										<a href="index.html">
+											<img class="scale-5 top-4 ml-4" alt="Porto" width="80" height="54" src="{{asset('porto/templates/img/demos/coffee/logo.png')}}">
+										</a>
+									</div>
+								</div>
 							</div>
-							<div class="overflow-hidden mb-2">
-								<h2 class="text-color-light font-weight-extra-bold text-13 line-height-1 line-height-sm-3 mb-0 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">ESPRESSO BAR</h2>
+							<div class="header-column justify-content-center align-items-end">
+								<div class="header-nav justify-content-lg-center p-0">
+									<div class="header-nav header-nav-links header-nav-light-text appear-animation" data-appear-animation="fadeInUpShorter">
+										<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-3 header-nav-main-sub-effect-1 header-nav-main-mobile-dark mr-lg-4">
+											<nav class="collapse">
+												<ul class="nav nav-pills flex-column flex-lg-row" id="mainNav">
+													<li class="dropdown order-1">
+														<a class="dropdown-item" data-hash data-hash-offset="92" href="#home">Home</a>
+													</li>
+													<li class="dropdown order-2">
+														<a class="dropdown-item" data-hash data-hash-offset="92" href="#about">About</a>
+													</li>
+													<li class="dropdown order-3">
+														<a class="dropdown-item" data-hash data-hash-offset="92" href="#reservations">Reservations</a>
+													</li>
+													<li class="dropdown order-5">
+														<a class="dropdown-item" data-hash data-hash-offset="92" href="#menu">Menu</a>
+													</li>
+													<li class="dropdown order-6">
+														<a class="dropdown-item" data-hash data-hash-offset="92" href="#news">News</a>
+													</li>
+													<li class="dropdown order-7">
+														<a class="dropdown-item" data-hash data-hash-offset="92" href="#locations">Locations</a>
+													</li>
+													<li class="align-items-center d-none d-lg-flex order-4 px-5 mx-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="250">
+														<span class="header-logo">
+															<a href="demo-coffee.html" class="w-100 text-center">
+																<img class="scale-4" alt="Porto - Demo Coffee" width="180" height="122" data-sticky-width="82" data-sticky-height="40" data-sticky-top="72" src="{{asset('porto/templates/img/demos/coffee/logo.png')}}">
+															</a>
+														</span>
+													</li>
+												</ul>
+											</nav>
+										</div>
+										<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
+											<i class="fas fa-bars"></i>
+										</button>
+									</div>
+								</div>
 							</div>
-							<p class="text-4-5 text-color-light font-weight-light text-center px-lg-5 mb-4" data-plugin-animated-letters data-plugin-options="{'startDelay': 1800, 'minWindowWidth': 0, 'animationSpeed': 30}">Since 2001 with you.</p>
-							<a href="#about" data-hash data-hash-offset="92" class="btn btn-primary btn-outline font-weight-bold text-color-light text-2 py-3 btn-px-4 mt-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="2700" data-plugin-options="{'minWindowWidth': 0}">LEARN ABOUT US</a>
+							<div class="header-column justify-content-end d-none d-lg-flex"></div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-		
-	</div>
-</div>
-<header id="header" class="header-narrow header-below-slider" data-plugin-options="{'stickyEnabled': true, 'stickyEnableOnBoxed': true, 'stickyEnableOnMobile': true, 'stickyStartAtElement': '#header', 'stickySetTop': '0', 'stickyChangeLogo': false}">
-	<div class="header-body border-0 box-shadow-none bg-dark appear-animation" data-appear-animation="customHeaderAnimOne">
-		<div class="header-container container">
-			<div class="header-row">
-				<div class="header-column justify-content-start d-lg-none">
-					<div class="header-row mt-2">
-						<div class="header-logo">
-							<a href="index.html">
-								<img class="scale-5 top-4 ml-4" alt="Porto" width="80" height="54" src="{{asset('porto/templates/img/demos/coffee/logo.png')}}">
-							</a>
-						</div>
-					</div>
-				</div>
-				<div class="header-column justify-content-center align-items-end">
-					<div class="header-nav justify-content-lg-center p-0">
-						<div class="header-nav header-nav-links header-nav-light-text appear-animation" data-appear-animation="fadeInUpShorter">
-							<div class="header-nav-main header-nav-main-square header-nav-main-dropdown-no-borders header-nav-main-effect-3 header-nav-main-sub-effect-1 header-nav-main-mobile-dark mr-lg-4">
-								<nav class="collapse">
-									<ul class="nav nav-pills flex-column flex-lg-row" id="mainNav">
-										<li class="dropdown order-1">
-											<a class="dropdown-item" data-hash data-hash-offset="92" href="#home">Home</a>
-										</li>
-										<li class="dropdown order-2">
-											<a class="dropdown-item" data-hash data-hash-offset="92" href="#about">About</a>
-										</li>
-										<li class="dropdown order-3">
-											<a class="dropdown-item" data-hash data-hash-offset="92" href="#reservations">Reservations</a>
-										</li>
-										<li class="dropdown order-5">
-											<a class="dropdown-item" data-hash data-hash-offset="92" href="#menu">Menu</a>
-										</li>
-										<li class="dropdown order-6">
-											<a class="dropdown-item" data-hash data-hash-offset="92" href="#news">News</a>
-										</li>
-										<li class="dropdown order-7">
-											<a class="dropdown-item" data-hash data-hash-offset="92" href="#locations">Locations</a>
-										</li>
-										<li class="align-items-center d-none d-lg-flex order-4 px-5 mx-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="250">
-											<span class="header-logo">
-												<a href="demo-coffee.html" class="w-100 text-center">
-													<img class="scale-4" alt="Porto - Demo Coffee" width="180" height="122" data-sticky-width="82" data-sticky-height="40" data-sticky-top="72" src="{{asset('porto/templates/img/demos/coffee/logo.png')}}">
-												</a>
-											</span>
-										</li>
-									</ul>
-								</nav>
-							</div>
-							<button class="btn header-btn-collapse-nav" data-toggle="collapse" data-target=".header-nav-main nav">
-								<i class="fas fa-bars"></i>
-							</button>
-						</div>
-					</div>
-				</div>
-				<div class="header-column justify-content-end d-none d-lg-flex"></div>
-			</div>
-		</div>
-	</div>
-</header>
+			</header>
 			
 			@yield('content')
 
@@ -368,8 +366,6 @@
 
 		</script>
 
-
-
 		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -381,7 +377,5 @@
 			ga('send', 'pageview');
 		</script>
 		 -->
-
-
 	</body>
 </html>
