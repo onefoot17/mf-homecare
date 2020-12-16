cd5ec6190f9736191c5da5af36f1a2cbc6979e10
Earl Misquitta <earl@vcompinc.com>
2020-12-16 17:54:07 -0500

    Slicing up Template, adding Porto default files

---
+++ resources/views/website/layouts/components/slider.blade.php
@@ -0,0 +1,19 @@
+<section class="mf__homepage-video section border-0 video overlay overlay-show overlay-op-8 m-0" data-video-path="website/assets/video/MF-Homecare-homepage-video" data-plugin-video-background data-plugin-options="{'posterType': 'jpg', 'position': '50% 50%'}" style="height: 100vh;">
+    <div class="container position-relative z-index-3 h-100">
+        <div class="row align-items-center h-100">
+            <div class="col">
+                <div class="d-flex flex-column align-items-center justify-content-center h-100 text-center">
+                    <h1 class="mf__homepage-video__heading-1 position-relative text-color-light font-weight-medium px-4 appear-animation" data-appear-animation="fadeInUpShorterPlus" data-plugin-options="{'minWindowWidth': 0}">Managing Family Homecare</h1>
+
+                    <h2 class="mf__homepage-video__heading-2 text-color-light appear-animation" data-appear-animation="blurIn" data-appear-animation-delay="1000" data-plugin-options="{'minWindowWidth': 0}">Affordable and Convenient</h2>
+
+                    <!-- <p class="text-4 text-color-light font-weight-light opacity-7 mb-0" data-plugin-animated-letters data-plugin-options="{'startDelay': 2000, 'minWindowWidth': 0}">Get Started - Register as a Caregiver</p> -->
+
+                    <!-- <a href="#" class="mf__homepage-video__heading-3" role="button">Get Started - Register as a Caregiver!</a> -->
+                </div>
+            </div>
+        </div>
+
+        <a href="#main" class="slider-scroll-button position-absolute bottom-10 left-50pct transform3dx-n50" data-hash data-hash-offset="80">Sroll To Bottom</a>
+    </div>
+</section>
---
+++ resources/views/website/layouts/footernav.blade.php
@@ -0,0 +1,115 @@
+<footer id="footer" class="mf__footer mt-0 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="300">
+    <div class="row">
+        <div class="mf__footer__inner container mx-md-auto">
+            <div class="mf__footer__col">
+                <ul class="mf__footer__col__inner">
+                    <li class="mf__footer__col__inner__li">
+                        <h4 class="mf__footer__col__inner__heading">{{ env( 'APP_NAME' ) }}</h4>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">How We Work</a>
+                        </p>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Register as a Caregiver</a>
+                        </p>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Register as a Client</a>
+                        </p>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Contact Us</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mf__footer__col">
+                <ul class="mf__footer__col__inner">
+                    <li class="mf__footer__col__inner__li">
+                        <h4 class="mf__footer__col__inner__heading">Information</h4>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Identity & Background Check</a>
+                        </p>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Cancellation Policy</a>
+                        </p>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Terms & Conditions</a>
+                        </p>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <p class="mf__footer__col__inner__p">
+                            <a class="mf__footer__col__inner__link" href="#">Privacy Policy</a>
+                        </p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mf__footer__col">
+                <ul class="mf__footer__col__inner">
+                    <li class="mf__footer__col__inner__li">
+                        <h4 class="mf__footer__col__inner__heading">Coming Soon</h4>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <a class="mf__app-icons-con" href="">
+                            <img class="mf__app-icons" src="{{ asset( 'website/assets/images/Available-on-the-App-Store.png' ) }}" />
+                        </a>
+                    </li>
+
+                    <li class="mf__footer__col__inner__li">
+                        <a class="mf__app-icons-con" href="">
+                            <img class="mf__app-icons" src="{{ asset( 'website/assets/images/Get-it-on-Google-Play.png' ) }}" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="mf__footer__col">
+                <ul class="mf__footer__col__inner mf__footer__socials">
+                    <li class="mf__footer__socials__social mf__footer__socials__social--first">
+                        <h4 class="mf__footer__col__inner__heading">Socials</h4>
+                    </li>
+
+                    <li class="mf__footer__socials__social">
+                        <a class="mf__footer__col__inner__link" href="#">
+                            <i class="fab fa-facebook"></i>
+                        </a>
+                    </li>
+
+                    <li class="mf__footer__socials__social">
+                        <a class="mf__footer__col__inner__link" href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+
+                    <li class="mf__footer__socials__social">
+                        <a class="mf__footer__col__inner__link" href="#">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
